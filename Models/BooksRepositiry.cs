fc90d9da63a8bbafb5df17e3311ee286981998d9
Nick Usov <nicky.yalta.ua@gmail.com>
2019-12-16 10:18:12 +0200

    books repositiry v1.0

---
+++ Models/BooksRepositiry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class BooksRepositiry
+    {
+        public static int Count;
+        static BooksRepositiry()
+        {
+            Count = 0;
+        }
+        public void Add(Book book)
+        {
+            Count++;
+        }
+    }
+}
